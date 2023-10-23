5aaf0d58f52fcd4a347b96ad3f243f4486cbc7df
010binary <augustine32fg@gmail.com>
2023-10-22 18:32:26 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -1,20 +1,43 @@
-#ifndef PUTKA_H
-#define PUTKA_H
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
+#include <limits.h>
 
-int putka(char c);
+#define BUFFER_SIZE 1024
+#define FLAG_PLUS (1 << 0)
+#define FLAG_SPACE (1 << 1)
+#define FLAG_HASH (1 << 2)
+
+int print_short_decimal(va_list args, int *count, int flags);
+int print_long_decimal(va_list args, int *count, int flags);
+int print_short_decimal(va_list args, int *count, int flags);
+int print_long_unsint(va_list args, int *count, int flags);
+int print_short_unsint(va_list args, int *count, int flags);
+int print_short_oct(va_list args, int *count, int flags);
+int print_long_oct(va_list args, int *count, int flags);
+int print_short_hex(va_list args, int *count, int flags);
+int print_long_hex(va_list args, int *count, int flags);
+int print_short_HEX(va_list args, int *count, int flags);
+int print_long_HEX(va_list args, int *count, int flags);
+int _string(char *str, int length);
+int print_address(va_list args, int *count);
+int print_hexad(unsigned long int n, int *count);
+int print_unsint(va_list args, int *count);
+int print_HEX(va_list args, int *count);
+int print_hex(va_list args, int *count);
+int print_oct(va_list args, int *count);
+int print_decimal(va_list args, int *count);
+int print_string(va_list args, int *count);
+int print_char(va_list args, int *count);
+int power(int base, int exponent);
+int _print_int(va_list argumts);
 int _printf(const char *format, ...);
-int print_num(va_list args);
-int print_string(va_list args);
-int handle_format(const char *format, va_list args);
-int print_binary(va_list args);
-int choose_specifier(char c, va_list args);
-int print_reverse(va_list args);
-int _strlen(char *s);
-char *_strchr(char *s, char c);
-int rot13(va_list args);
-int print_address(va_list args);
+int _benedict(const char *c);
+int _putc(char c);
 
-#endif
+#endif /* MAIN_H */
